fa93546db96e238e8cb1a175357d887ba33ca628
xolor <gebratene.ente+github@gmail.com>
2015-06-19 22:23:52 +0200

    Updated StorageService draft

--- metadict-core/src/main/java/org/xlrnet/metadict/core/storage/StorageService.java
+++ metadict-core/src/main/java/org/xlrnet/metadict/core/storage/StorageService.java
@@ -30,32 +30,59 @@ import java.util.Optional;
 
 /**
  * Main interface for accessing Metadict storage services. The storage service provides CRUD operations on a simple
- * key-value store. Each key may be used one time for each class.
+ * key-value store with namespaces. Each key can be used exactly one time per namespace. When using CRUD operations,
+ * all attributes of the objects will be (de-)serialized. Namespaces can be used for separating different types of
+ * data.
  * <p>
- * E.g.: There may be only one value with key "123" for the Class "Sample". However you can save another value with
- * key "123" for the Class "Test". The canonical class name should be used for internal indexes/namespaces.
+ * When connecting to an external storage service, the stored data will be available globally for each connected
+ * client. This may be helpful for implementing persistent data storage across multiple Metadict instances.
  */
 public interface StorageService {
 
     /**
-     * Store a new value of any type under a given key. The targeted index
+     * Store a new value of any type in the requested namespace with a given key. This method will throw a {@link
+     * StorageException} if there is already an object with the same key under in the same namespace.
+     * <p>
+     * Upon saving, all private attributes will be extracted and stored in the storage backend. It depends on the
+     * concrete backend implementation, whether the given class must be {@link java.io.Serializable}. However, it is
+     * recommended to store only serializable class to avoid problems.
      *
+     * @param namespace
+     *         The name of the namespace in which the key should be placed. Must be a non-empty and non-null string.
      * @param key
+     *         The key for the new object. Must be a non-empty and non-null string.
      * @param value
+     *         The object that should be stored under the given key in the given namespace.
      * @param <T>
-     * @return
-     * @throws IllegalArgumentException
+     *         The class to store. Its canonical class name should be used for internal indexes/namespaces of the
+     *         backend.
+     * @return The input object if it could be saved - never null. In case of any error situations, an exception will be
+     * thrown.
+     * @throws StorageException
      */
     @NotNull
-    <T> T create(@NotNull String key, @NotNull T value) throws IllegalArgumentException;
+    <T> T create(@NotNull String namespace, @NotNull String key, @NotNull T value) throws StorageException;
+
+    /**
+     * Deletes the associated value for a given key in the given namespace. After deleting, the associated value will
+     * not be accessible anymore by read or update operations.
+     *
+     * @param namespace
+     *         The name of the namespace in which the key lies. Must be a non-empty and non-null string.
+     * @param key
+     *         The key for the new object. Must be a non-empty and non-null string.
+     * @return True, if any value was deleted or false if none. A false return value indicates always that no value
+     * could be found in the given namespace for the given key. In case of an internal backend error, an exception will
+     * be thrown.
+     * @throws StorageException
+     *         Will be thrown if any backend errors occured.
+     */
+    boolean delete(@NotNull String namespace, @NotNull String key) throws StorageException;
 
     @NotNull
-    <T> Optional<T> read(@NotNull String key, Class<T> clazz) throws IllegalArgumentException;
+    <T> Optional<T> read(@NotNull String namespace, @NotNull String key, Class<T> clazz) throws StorageException;
 
     @NotNull
-    <T> T update(@NotNull String key, @NotNull T newValue) throws IllegalArgumentException;
-
-    @NotNull
-    <T> T delete(String key) throws IllegalArgumentException;
+    <T> T update(@NotNull String namespace, @NotNull String key, @NotNull T newValue) throws StorageException;
 
 }
