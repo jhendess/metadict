af0fe00f41a5c271810b0032647c72738ede29ae
jhendess <gebratene.ente+github@gmail.com>
2015-07-15 23:42:38 +0200

    Basic setup for woxikon engine

---
+++ metadict-engines/woxikon-engine/src/main/java/org/xlrnet/metadict/engines/woxikon/WoxikonEngineProvider.java
@@ -0,0 +1,118 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Jakob Hendeß
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.xlrnet.metadict.engines.woxikon;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.xlrnet.metadict.api.engine.AutoTestSuite;
+import org.xlrnet.metadict.api.engine.SearchEngine;
+import org.xlrnet.metadict.api.engine.SearchProvider;
+import org.xlrnet.metadict.api.language.BilingualDictionary;
+import org.xlrnet.metadict.api.language.Language;
+import org.xlrnet.metadict.api.metadata.EngineDescription;
+import org.xlrnet.metadict.api.metadata.EngineDescriptionBuilder;
+import org.xlrnet.metadict.api.metadata.FeatureSet;
+import org.xlrnet.metadict.api.metadata.FeatureSetBuilder;
+
+/**
+ * Provider with a search engine for searching in various dictionaries on {@see <a
+ * href="http://www.woxikon.de/">woxikon.de</a>}.
+ */
+public class WoxikonEngineProvider implements SearchProvider {
+
+    /**
+     * Return a set of user-defined automatic test cases. See {@link AutoTestCase} for more information. To enable
+     * automatic testing, the provided {@link FeatureSet} has to return <i>true</i> when calling {@link
+     * FeatureSet#isSupportsSelfTest()}.
+     *
+     * @return a set of user-defined automatic test cases.
+     */
+    @Nullable
+    @Override
+    public AutoTestSuite getAutoTestSuite() throws Exception {
+        return null;
+    }
+
+    /**
+     * Return an {@link EngineDescription} object that contains descriptive i.e. textual information about the
+     * underlying engine. This can be e.g.the name, url, etc. of the engine.
+     *
+     * @return an object that contains descriptive i.e. textual information about the underlying engine.
+     */
+    @NotNull
+    @Override
+    public EngineDescription getEngineDescription() {
+        return new EngineDescriptionBuilder()
+                .setEngineName("Unofficial woxikon.de Engine")
+                .setAuthorName("xolor")
+                .setSearchBackendLink("http://www.woxikon.de/")
+                .setSearchBackendName("woxikon.de")
+                .build();
+    }
+
+    /**
+     * Return a {@link FeatureSet} object that contains information about which features the underlying engine supports.
+     * This includes e.g. the supported languages of the engine.
+     *
+     * @return an object that contains information about which features the underlying engine supports.
+     */
+    @NotNull
+    @Override
+    public FeatureSet getFeatureSet() {
+        return new FeatureSetBuilder()
+                .setProvidesBilingualDictionaryEntries(true)
+                .setProvidesMonolingualEntries(true)
+                .setProvidesAlternatives(true)
+                .setProvidesExternalContent(false)
+                .setSupportsAutoTest(false)
+                .setSupportsFuzzySearch(true)
+                .addSupportedBilingualDictionary(BilingualDictionary.fromLanguages(Language.GERMAN, Language.FRENCH, true))
+                .addSupportedBilingualDictionary(BilingualDictionary.fromLanguages(Language.GERMAN, Language.SPANISH, true))
+                .addSupportedBilingualDictionary(BilingualDictionary.fromLanguages(Language.GERMAN, Language.SWEDISH, true))
+                .addSupportedBilingualDictionary(BilingualDictionary.fromLanguages(Language.GERMAN, Language.RUSSIAN, true))
+                .addSupportedBilingualDictionary(BilingualDictionary.fromLanguages(Language.GERMAN, Language.FINNISH, true))
+                .addSupportedBilingualDictionary(BilingualDictionary.fromLanguages(Language.GERMAN, Language.TURKISH, true))
+                .addSupportedBilingualDictionary(BilingualDictionary.fromLanguages(Language.GERMAN, Language.ENGLISH, true))
+                .addSupportedBilingualDictionary(BilingualDictionary.fromLanguages(Language.GERMAN, Language.ITALIAN, true))
+                .addSupportedBilingualDictionary(BilingualDictionary.fromLanguages(Language.GERMAN, Language.DUTCH, true))
+                .addSupportedBilingualDictionary(BilingualDictionary.fromLanguages(Language.GERMAN, Language.PORTUGUESE, true))
+                .addSupportedBilingualDictionary(BilingualDictionary.fromLanguages(Language.GERMAN, Language.POLISH, true))
+                .addSupportedBilingualDictionary(BilingualDictionary.fromLanguages(Language.GERMAN, Language.NORWEGIAN, true))
+                .addSupportedLexicographicLanguage(Language.GERMAN)
+                .build();
+    }
+
+    /**
+     * Create a new instance of the search engine. The returned engine should not be stateful and has to be implemented
+     * thread-safe.
+     *
+     * @return a new instance of the search engine.
+     */
+    @NotNull
+    @Override
+    public SearchEngine newEngineInstance() {
+        return new WoxikonEngine();
+    }
+}
