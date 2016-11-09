f1fcec6f6e1e99aa6d65ada4f2ed17aee4b4813a
xolor <gebratene.ente+github@gmail.com>
2016-11-09 21:00:36 +0100

    META-76: Removed unnecessary client dependencies

--- metadict-ui/test/karma.conf.js
+++ metadict-ui/test/karma.conf.js
@@ -49,10 +49,7 @@ module.exports = function(config) {
       // bower:js
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
-      'bower_components/angular-animate/angular-animate.js',
-      'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-touch/angular-touch.js',
       'bower_components/lodash/lodash.js',
       'bower_components/restangular/dist/restangular.js',
       'bower_components/materialize/bin/materialize.js',
